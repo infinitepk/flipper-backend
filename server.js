beec3a5d6f776df38ed345b0a2459ac2984901ba
infinitepk <tikanipavankumar@gmail.com>
2026-07-28 21:23:32 +0530

    Use Render port

--- server.js
+++ server.js
@@ -23,8 +23,10 @@ app.post('/article', async (req, res) => {
     res.send('Coming Soon');
 });
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 
     startScheduler();
 });
